ab1c96657deb9a8c12aaf3a4856ec95c6ecf7b38
Ali <aliafsah1988@gmail.com>
2019-12-12 20:03:55 +0330

    constance styles utils added

---
+++ src/constants/contactInfo.js
@@ -0,0 +1,4 @@
+export default {
+    phone: '+98 9374055403',
+    email: 'ali.afsah1988@gmail.com'
+}
---
+++ src/constants/urls.js
@@ -0,0 +1,5 @@
+export default {
+  facebook: "https://www.facebook.com/profile.php?id=100001857186844",
+  linkedin: "https://www.linkedin.com/in/aliafsahnoudeh/",
+  github: "https://github.com/aliafsah1988/"
+};
